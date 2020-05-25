2414732ff6c66a24abc8fae5a15e7c464aa6255b
AtikinEvil <65335787+AtikinEvil@users.noreply.github.com>
2020-05-26 00:33:30 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,18 +2,20 @@ function callModal() {
     let modalTrigger = document.querySelector('.btn'),
         modal = document.querySelector('.modal'),
         modalClose = document.querySelector('.modal__close');
+        
+    modalTrigger.onclick = function() {
+        modal.classList.add('active')
+    };
 
-    modalTrigger.addEventListener('click', () => {
-        modal.classList.add('active');
-    });
-
-    modal.addEventListener('click', (e) => {
+    modal.onclick = function(e)  {
         if (e.target === modal) {
-            modal.classList.remove('active');
+            modal.classList.remove('active')
         } else if (e.target === modalClose) {
-            modal.classList.remove('active');
-        };
-    });
-};
+            modal.classList.remove('active')
+        }
+    };
+}
 
 callModal()
+
+/* хотел вместо onclick использовать addEventListener, но он не работал в explorer */
