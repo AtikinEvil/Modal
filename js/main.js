23e3b41fb6288317a5cfc9e11ef3bab2bff9d4f7
AtikinEvil <65335787+AtikinEvil@users.noreply.github.com>
2020-05-25 23:32:57 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function callModal() {
+    let modalTrigger = document.querySelector('.btn'),
+        modal = document.querySelector('.modal'),
+        modalClose = document.querySelector('.modal__close');
+
+    modalTrigger.addEventListener('click', () => {
+        modal.classList.add('active');
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            modal.classList.remove('active')
+        } else if (e.target === modalClose) {
+            modal.classList.remove('active')
+        }
+    });
+}
+
+callModal()
